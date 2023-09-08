3b2a1295347606658d3a507a9481464f832b73cf
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-08 10:26:25 +0330

    update: namespace interface

--- pkg/models/namespace/interface.go
+++ pkg/models/namespace/interface.go
@@ -10,9 +10,9 @@ import (
 type Interface interface {
 	Create(namespace *Namespace) error
 	Delete(namespaceID uint) error
-	Get() ([]*Namespace, error)
-	GetByID(namespaceID uint) (*Namespace, error)
-	GetByIDs(namespaceIDs []uint) ([]*Namespace, error)
+	Update(id uint, namespace *Namespace) error
+	GetAll() ([]*Namespace, error)
+	GetByID(id uint) (*Namespace, error)
 }
 
 func New(db *gorm.DB) Interface {
@@ -29,11 +29,15 @@ func (c core) Create(namespace *Namespace) error {
 	return c.db.Create(namespace).Error
 }
 
+func (c core) Update(id uint, namespace *Namespace) error {
+	return c.db.Where("id = ?", id).Updates(namespace).Error
+}
+
 func (c core) Delete(namespaceID uint) error {
 	return c.db.Delete(&Namespace{}, "id = ?", namespaceID).Error
 }
 
-func (c core) Get() ([]*Namespace, error) {
+func (c core) GetAll() ([]*Namespace, error) {
 	list := make([]*Namespace, 0)
 
 	if err := c.db.Find(&list).Error; err != nil {
@@ -43,26 +47,16 @@ func (c core) Get() ([]*Namespace, error) {
 	return list, nil
 }
 
-func (c core) GetByID(namespaceID uint) (*Namespace, error) {
+func (c core) GetByID(id uint) (*Namespace, error) {
 	namespace := new(Namespace)
 
-	if err := c.db.Preload("Projects").Where("id = ?", namespaceID).First(&namespace).Error; err != nil {
+	if err := c.db.Preload("Projects").Where("id = ?", id).First(&namespace).Error; err != nil {
 		return nil, fmt.Errorf("[db.Namespace.GetByID] failed to get record error=%w", err)
 	}
 
-	if namespace.ID != namespaceID {
+	if namespace.ID != id {
 		return nil, ErrRecordNotFound
 	}
 
 	return namespace, nil
-}
-
-func (c core) GetByIDs(namespaceIDs []uint) ([]*Namespace, error) {
-	list := make([]*Namespace, 0)
-
-	if err := c.db.Where("id in ?", namespaceIDs).Find(&list).Error; err != nil {
-		return nil, fmt.Errorf("[db.Namespace.Get] failed to get records error=%w", err)
-	}
-
-	return list, nil
 }
