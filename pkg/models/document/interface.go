9e1a10c74bcec265f38f64cc74aa7d424b8a768b
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-01 12:30:11 +0330

    update: interface of document

--- pkg/models/document/interface.go
+++ pkg/models/document/interface.go
@@ -1,12 +1,17 @@
 package document
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 // Interface manages the documents methods
 type Interface interface {
 	Create(document *Document) error
 	Update(document *Document) error
 	Delete(projectID uint) error
+	GetByID(id uint) (*Document, error)
 }
 
 func New(db *gorm.DB) Interface {
@@ -29,4 +34,19 @@ func (c core) Update(document *Document) error {
 
 func (c core) Delete(projectID uint) error {
 	return c.db.Delete(&Document{}, "project_id = ?", projectID).Error
+}
+
+func (c core) GetByID(id uint) (*Document, error) {
+	document := new(Document)
+
+	query := c.db.First(&document, "id = ?", id)
+	if err := query.Error; err != nil {
+		return nil, fmt.Errorf("[db.Document.GetByID] failed to get record error=%w", err)
+	}
+
+	if document.ID != id {
+		return nil, ErrDocumentNotFound
+	}
+
+	return document, nil
 }
