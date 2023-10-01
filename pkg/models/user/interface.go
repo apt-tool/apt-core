6cd1667554b7246e625c190f72757368cec2166c
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-01 12:23:33 +0330

    update: interface of users

--- pkg/models/user/interface.go
+++ pkg/models/user/interface.go
@@ -12,11 +12,7 @@ import (
 type Interface interface {
 	Create(user *User) error
 	Delete(id uint) error
-	Update(id uint, user *User) error
 	GetAll() ([]*User, error)
-	GetByID(id uint) (*User, error)
-	GetByName(name string) (*User, error)
-	GetByIDs(userIDs []uint) ([]*User, error)
 	Validate(name, pass string) (*User, error)
 }
 
@@ -30,20 +26,19 @@ type core struct {
 	db *gorm.DB
 }
 
+// Create a new user
 func (c core) Create(user *User) error {
 	user.Password = crypto.GetMD5Hash(user.Password)
 
 	return c.db.Create(user).Error
 }
 
+// Delete an existed user
 func (c core) Delete(userID uint) error {
 	return c.db.Delete(&User{}, "id = ?", userID).Error
 }
 
-func (c core) Update(userID uint, user *User) error {
-	return c.db.Where("id = ?", userID).Updates(user).Error
-}
-
+// GetAll users
 func (c core) GetAll() ([]*User, error) {
 	list := make([]*User, 0)
 
@@ -54,49 +49,12 @@ func (c core) GetAll() ([]*User, error) {
 	return list, nil
 }
 
-func (c core) GetByID(id uint) (*User, error) {
-	user := new(User)
-
-	if err := c.db.Where("id = ?", id).First(&user).Error; err != nil {
-		return nil, fmt.Errorf("[db.User.Get] failed to get records error=%w", err)
-	}
-
-	if user.ID != id {
-		return nil, ErrUserNotFound
-	}
-
-	return user, nil
-}
-
-func (c core) GetByName(name string) (*User, error) {
-	user := new(User)
-
-	if err := c.db.Where("username = ?", name).First(&user).Error; err != nil {
-		return nil, fmt.Errorf("[db.User.Get] failed to get records error=%w", err)
-	}
-
-	if user.Username != name {
-		return nil, ErrUserNotFound
-	}
-
-	return user, nil
-}
-
-func (c core) GetByIDs(userIDs []uint) ([]*User, error) {
-	list := make([]*User, 0)
-
-	if err := c.db.Where("id in ?", userIDs).Find(&list).Error; err != nil {
-		return nil, fmt.Errorf("[db.User.Get] failed to get records error=%w", err)
-	}
-
-	return list, nil
-}
-
+// Validate a user by its credential
 func (c core) Validate(name, pass string) (*User, error) {
 	user := new(User)
 
 	if err := c.db.Where("username = ?", name).First(&user).Error; err != nil {
-		return nil, fmt.Errorf("[db.User.Validate] failed to get user error=%w", err)
+		return nil, ErrUserNotFound
 	}
 
 	if user.Username != name || user.Password != crypto.GetMD5Hash(pass) {
--- pkg/models/user/user.go
+++ pkg/models/user/user.go
@@ -1,15 +1,10 @@
 package user
 
-import (
-	"github.com/ptaas-tool/base-api/pkg/enum"
-
-	"gorm.io/gorm"
-)
+import "gorm.io/gorm"
 
 // User is the base entity of our clients
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique"`
 	Password string
-	Role     enum.Role
 }
